8f8e87c77ccfa1c47ed07e916a69ccb36f125bec
Asraf Uddin Ahmed <13ratul@gmail.com>
2023-04-04 13:39:11 +0200

    TASK_1_WIP: Bootstrapping the initial structure of the project

---
+++ src/main/java/com/epam/distributedlibraryservice/DistributedLibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.distributedlibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedLibraryServiceApplication.class, args);
+	}
+
+}
