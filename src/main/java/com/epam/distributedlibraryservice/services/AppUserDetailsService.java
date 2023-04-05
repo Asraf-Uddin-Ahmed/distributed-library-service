4a8e3adac45b48fb506c9c284af03be7922e7434
Asraf Uddin Ahmed <13ratul@gmail.com>
2023-04-05 19:31:34 +0200

    TASK_3_WIP: Prepare AppUserDetails for Spring Security

---
+++ src/main/java/com/epam/distributedlibraryservice/services/AppUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.epam.distributedlibraryservice.services;
+
+import com.epam.distributedlibraryservice.entities.User;
+import com.epam.distributedlibraryservice.models.AppUserDetailsModel;
+import com.epam.distributedlibraryservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new AppUserDetailsModel(user);
+    }
+
+}
