9cad6f14be93fd5e0a37b312c9cf6ffc498bcd2d
Asraf Uddin Ahmed <13ratul@gmail.com>
2023-04-06 12:17:06 +0200

    TASK_3_WIP: Insert some user data

---
+++ src/main/resources/db/migration/V1.05__Insert_user_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user`
+(`username`, `name`, `email`, `password`, `user_status`, `creation_time`, `update_time`) VALUES
+('user1', 'user1', 'user1@gmail.com', /*111*/ '$2a$08$0pcAnwqdBySj4ARDS6m/WuY09y6mkmCI6cjbYXYKnOmmxANLJPP06', 'ACTIVE', '2023-04-05 18:44:25', '2023-04-05 18:44:25'),
+('user2', 'user2', 'user2@gmail.com', /*123456*/ '$2a$08$JIq9LYVaeOBYL.GZNkt4h.RnmwMVDtoTCKKL3q7Pf6CunjO8N/PJO', 'ACTIVE', '2023-04-05 19:07:32', '2023-04-05 19:07:32')
+;
