c3867c36ec82178df18bdfd0f0561dd79296f57b
Asraf Uddin Ahmed <13ratul@gmail.com>
2023-04-06 12:11:31 +0200

    TASK_3_WIP: Update SpEL based validator to fetch message from message.properties

--- src/main/java/com/epam/distributedlibraryservice/validators/SpELClassValidator.java
+++ src/main/java/com/epam/distributedlibraryservice/validators/SpELClassValidator.java
@@ -2,16 +2,25 @@ package com.epam.distributedlibraryservice.validators;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
+import java.util.Locale;
+
 
 public class SpELClassValidator implements ConstraintValidator<ValidateClassExpression, Object> {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	private ValidateClassExpression annotation;
 	private ExpressionParser parser = new SpelExpressionParser();
 
+	@Override
 	public void initialize(ValidateClassExpression constraintAnnotation) {
 		annotation = constraintAnnotation;
 		parser.parseExpression(constraintAnnotation.value());
@@ -19,7 +28,14 @@ public class SpELClassValidator implements ConstraintValidator<ValidateClassExpr
 
 	public boolean isValid(Object value, ConstraintValidatorContext context) {
 		StandardEvaluationContext spelContext = new StandardEvaluationContext(value);
-		return (Boolean) parser.parseExpression(annotation.value()).getValue(spelContext);
+		boolean isValid = (Boolean) parser.parseExpression(annotation.value()).getValue(spelContext);
+		if(!isValid) {
+			// COPILOT has generated this awesome code
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(messageSource.getMessage(annotation.message(), null, Locale.getDefault()))
+					.addPropertyNode(annotation.appliedFields()[0]).addConstraintViolation();
+		}
+		return isValid;
 	}
 
 }
