fb61e110184935a834e64b55f58d6ba709ca2925
Asraf Uddin Ahmed <13ratul@gmail.com>
2023-04-06 12:08:41 +0200

    TASK_3_WIP: Update security config for Oauth and update deprecated implementation as well

--- src/main/java/com/epam/distributedlibraryservice/configs/SecurityConfig.java
+++ src/main/java/com/epam/distributedlibraryservice/configs/SecurityConfig.java
@@ -1,54 +1,54 @@
 package com.epam.distributedlibraryservice.configs;
 
-import com.epam.distributedlibraryservice.repositories.UserRepository;
-import com.epam.distributedlibraryservice.services.AppUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
+@Order(1)
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     @Autowired
-    private UserRepository userRepository;
+    private UserDetailsService appUserDetailsService;
+    @Autowired
+    private AuthenticationSuccessHandler successHandler;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(getDaoAuthProvider());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").authenticated()
-                .antMatchers("/**").permitAll()
-                .and().formLogin().loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/")
-                .and().csrf().disable();
-
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new AppUserDetailsService(userRepository);
-    }
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/register/**", "/login/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").successHandler(successHandler)
+                .and().csrf().disable()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/login")
+                .and().oauth2Login().loginPage("/login").successHandler(successHandler);
+        return http.build();
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(8);
     }
 
     @Bean
     public DaoAuthenticationProvider getDaoAuthProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(appUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
         return daoAuthenticationProvider;
     }
 
